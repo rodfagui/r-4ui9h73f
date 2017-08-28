fe5f2d18662fdc7c3a9f0ea224595c445599b1ab
Rodman Aguirre <rodfagui@gmail.com>
2017-08-27 23:53:41 -0500

    Finish

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { value: '' };
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.repeat.bind(this)} />
+        <p className="repeater">{this.state.value}</p>
       </div>
     );
   }
+  repeat(event) {
+    this.setState({
+      value: event.target.value
+    });
+  }
+
 }
 
 export default App;
